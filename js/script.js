28b35026f27539c3d21682b10fff41f33abfa1bf
danbender <mail@danbender.net>
2014-06-29 17:41:04 +0200

    call api and store in session to keep api requests below limit of 150

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 var apiKey  = 'oO3cnEECOzCm9o2qp3OdVa0A1z0gfZx2';
 var userID  = 'danbender';
+
+
+(function() {
+    var behanceUserAPI = 'http://www.behance.net/v2/users/'+ userID +'?callback=?&api_key='+ apiKey;
+    function setUserTemplate() {
+        var userData    = JSON.parse(sessionStorage.getItem('behanceUser')),
+        getTemplate = $('#profile-template').html(),
+        template    = Handlebars.compile(getTemplate),
+        result      = template(userData);
+        $('#header').html(result);
+    };
+    if(sessionStorage.getItem('behanceUser')) {
+        setUserTemplate();
+    } else {
+        $.getJSON(behanceUserAPI, function(user) {
+            var data = JSON.stringify(user);
+            sessionStorage.setItem('behanceUser', data);
+            setUserTemplate();
+        });
+    };
+})();
