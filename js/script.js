0954d110c58b2e170c5b59e6ec9d84fb5b04d545
danbender <mail@danbender.net>
2014-07-07 21:40:19 +0200

    js for lightbox and local storage

--- js/script.js
+++ js/script.js
@@ -43,3 +43,43 @@ var userID  = 'danbender';
         });
     };
 })();
+
+
+
+//
+
+$('#portfolio').on('click', '.portfolio-cover', function() {
+    var $this = $(this),
+        projectID = $this.data('project-id'),
+        beProjectContentAPI = 'http://www.behance.net/v2/projects/'+ projectID +'?callback=?&api_key=' + apiKey,
+        keyName = 'behanceProjectImages-' + projectID;
+
+    function showGallery(dataSource) {
+        $this.magnificPopup({
+            items: dataSource,
+            gallery: {
+                enabled: true
+            },
+            type: 'image',
+            mainClass: 'animated',
+            removalDelay: 350
+    }).magnificPopup('open');
+};
+
+    if(localStorage.getItem(keyName)) {
+        var srcItems = JSON.parse(localStorage.getItem(keyName));
+        showGallery(srcItems);
+    } else {
+        $.getJSON(beProjectContentAPI, function(projectContent) {
+            var src = [];
+            $.each(projectContent.project.modules, function(index, mod) {
+                if(mod.src != undefined) {
+                    src.push({ src: mod.src });
+                }
+            });
+            showGallery(src);
+            var data = JSON.stringify(src);
+            localStorage.setItem(keyName, data);
+        });
+    };
+});
